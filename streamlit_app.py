6c79cb6ead17e981851b0a6faf417c92f5a14eb8
rmatheson-snowflake <118143516+rmatheson-snowflake@users.noreply.github.com>
2022-11-25 10:16:04 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,5 +25,9 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+
+# Take the json version of the response and normalise it
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Output it to the screen as a table
+streamlit.dataframe(fruityvice_normalized)
